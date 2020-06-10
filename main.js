7754e75bdc43b9b4210e9b9fba4a36e46a0f0590
sarikadara <sarika.dara96@gmail.com>
2020-06-10 16:15:43 +0530

    arrow

---
+++ main.js
@@ -0,0 +1,30 @@
+"use strict"
+/*
+function Prefixer(prefix){
+  this.prefix = prefix;
+}
+Prefixer.prototype.prefixArray = function(arr){
+  return arr.map((x) => {
+    console.log(this.prefix + x);
+  });
+}
+let pre = new Prefixer('Hello ');
+pre.prefixArray(['dara', 'sarika']);
+*/
+
+/*
+let add = function(a,b){
+  let sum = a+b;
+  console.log(sum);
+  return false;
+}
+add(3,5);
+*/
+
+let add = (a,b) => {
+  let sum = a+b;
+  console.log(sum);
+  return false;
+}
+
+add(6,5);
